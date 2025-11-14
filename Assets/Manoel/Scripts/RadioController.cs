2f491d3e2a49bae30105eccf3448d591498db78a
ManoelEnder <manoel.ender@alunos.sc.senac.br>
2025-11-14 11:23:48 -0300

    Radio Test

---
+++ Assets/Manoel/Scripts/RadioController.cs
@@ -0,0 +1,58 @@
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using System.Collections;
+
+public class RadioController : MonoBehaviour
+{
+    public AudioSource murmurSource;
+    public string[] legendas;
+    public TextMeshProUGUI subtitleText;
+    public float tempoLegenda = 4f;
+    public float delayEntreMensagens = 3f;
+    public Button botaoRadio;
+
+    bool jaTocou = false;
+
+    void Start()
+    {
+        if (botaoRadio != null)
+            botaoRadio.onClick.AddListener(AcionarRadio);
+
+        if (murmurSource != null && murmurSource.clip != null)
+        {
+            murmurSource.loop = true;
+            murmurSource.volume = 1f;
+            murmurSource.Play();
+        }
+    }
+
+    void AcionarRadio()
+    {
+        if (!jaTocou)
+        {
+            jaTocou = true;
+            StartCoroutine(MostrarMensagens());
+        }
+    }
+
+    IEnumerator MostrarMensagens()
+    {
+        for (int i = 0; i < legendas.Length; i++)
+        {
+            if (murmurSource != null)
+                murmurSource.volume = 0.25f;
+
+            subtitleText.text = legendas[i];
+
+            yield return new WaitForSeconds(tempoLegenda);
+
+            subtitleText.text = "";
+
+            if (murmurSource != null)
+                murmurSource.volume = 1f;
+
+            yield return new WaitForSeconds(delayEntreMensagens);
+        }
+    }
+}
